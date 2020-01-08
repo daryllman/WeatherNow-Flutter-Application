fb9df74aee0f74fd97e04c6f7ea6000b0c8eccc8
Daryll Wong <daryll_wong@hotmail.com>
2020-01-08 22:17:12 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/daryllwong/weather_now_flutter_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.daryllwong.weather_now_flutter_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
