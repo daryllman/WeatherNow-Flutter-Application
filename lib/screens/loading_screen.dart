70b253bffed1985bbec3b76c0fc31e9dedb0ec08
Daryll Wong <daryll_wong@hotmail.com>
2020-01-15 18:25:09 +0800

    parse json payload

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -22,9 +23,30 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
     print(location.latitude);
   }
+  
+  void getData() async {
+    http.Response response = await http.get('http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    if (response.statusCode == 200){
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      double temperature = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+      print(temperature);
+      print(condition);
+      print(cityName);
+
+    } else{
+      print(response.body);
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
